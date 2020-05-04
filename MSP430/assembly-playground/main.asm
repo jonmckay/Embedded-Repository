295a9943689941b39192ec8eb563718db6569833
Jon McKay <jmckay0411@gmail.com>
2020-05-03 20:15:39 -0600

    Add assembly project to blink LED on MSP430 board

---
+++ MSP430/assembly-playground/main.asm
@@ -0,0 +1,51 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+	; Set GPIO P1.0 to be an output
+	BIS.B#1, &PADIR_L
+
+	; Set output on GPIO P1.0 to high voltage
+	BIS.B#1, &PAOUT_L
+
+	; Set output on GPIO P1.0 to low voltage
+	BIC.B#1, &PAOUT_L
+
+	; And repeat.
+	BIS.B#1, &PAOUT_L
+	BIC.B#1, &PAOUT_L
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
