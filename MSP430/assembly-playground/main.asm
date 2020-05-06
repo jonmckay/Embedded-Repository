1c8d83ff3baa3c7c49bcc9747313f28ad37160fa
Jon McKay <jmckay0411@gmail.com>
2020-05-06 16:31:16 -0600

    Configure GPIO inputs to blink LED when pressing button

--- MSP430/assembly-playground/main.asm
+++ MSP430/assembly-playground/main.asm
@@ -24,32 +24,49 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; Main loop here
 ;-------------------------------------------------------------------------------
 
+;-------------------------------------------------------------------------------
+; Program to blink LED on GPIO P1.0
+;-------------------------------------------------------------------------------
 	; Set GPIO P1.0 to be an output
-	BIS.B#1, &PADIR_L
+	;BIS.B#1, &PADIR_L
 
-MainLoop:
+;MainLoop:
 
 	; Set output on GPIO P1.0 to high voltage
-	BIS.B#1, &PAOUT_L
+	;BIS.B#1, &PAOUT_L
 
-	MOV #50000, R4 ; works out to a delay of about 0.2 seconds
+	;MOV #50000, R4 ; works out to a delay of about 0.2 seconds
 
-DelayOn:
+;DelayOn:
 
-	SUB #1, R4 ; subtract 1 from R4
-	CMP #0, R4 ; compare R4 to 0
-	JNZ DelayOn ; while R4 != 0, keep running DelayOn loop
+	;SUB #1, R4 ; subtract 1 from R4
+	;CMP #0, R4 ; compare R4 to 0
+	;JNZ DelayOn ; while R4 != 0, keep running DelayOn loop
 
 	; Set output on GPIO P1.0 to low voltage
-	BIC.B#1, &PAOUT_L
+	;BIC.B#1, &PAOUT_L
 
-	MOV #50000, R4
+	;MOV #50000, R4
 
-DelayOff:
+;DelayOff:
 
-	SUB #1, R4
-	CMP #0, R4
-	JNZ DelayOff
+	;SUB #1, R4
+	;CMP #0, R4
+	;JNZ DelayOff
+
+	;JMP MainLoop
+
+;-------------------------------------------------------------------------------
+; Program to set GPIO P1.0 to an output (LED) and GPIO P1.1 to an input (button)
+;-------------------------------------------------------------------------------
+
+	; Set GPIO P1.0 to be an output (PADIR bit == 1)
+	BIS.B	#1, &PADIR_L
+
+	; Set GPIO P1.1 to be an input (PADIR bit == 0)
+	BIC.B	#2, &PADIR_L
+
+MainLoop:	; infinite loop that does nothing
 
 	JMP MainLoop
 
