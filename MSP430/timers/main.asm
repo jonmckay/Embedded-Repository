fedab7a346d7a67601cc30ab333bcbd177efd23b
Jon McKay <jmckay0411@gmail.com>
2020-05-12 13:18:56 -0600

    Start timer LED blinking project by enabling general interrupts

---
+++ MSP430/timers/main.asm
@@ -0,0 +1,46 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Program designed to use timers to blink a LED on the MSP430 board
+;-------------------------------------------------------------------------------
+
+	; Set GPIO P1.0 to be an output
+	BIS.B	#1, &PADIR_L
+
+	; Enable interrupts
+	NOP
+	BIS		#GIE, SR
+	NOP
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
