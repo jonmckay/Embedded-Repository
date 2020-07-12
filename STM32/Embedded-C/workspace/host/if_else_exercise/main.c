4911b577ab380381aaf7ecf7768726e4c6170b2c
Jon McKay <jmckay0411@gmail.com>
2020-07-12 11:11:41 -0600

    Modify if else exercise to take in real numbers and compare those

--- STM32/Embedded-C/workspace/host/if_else_exercise/main.c
+++ STM32/Embedded-C/workspace/host/if_else_exercise/main.c
@@ -10,25 +10,34 @@
 
 int main(void)
 {
-	int32_t firstNumber = 0;
-	int32_t secondNumber = 0;
+	float firstNumber = 0;
+	float secondNumber = 0;
 
 	printf("Enter a number(integer):");
-	scanf("%d", &firstNumber);
+	scanf("%f", &firstNumber);
 
 	printf("Enter a second number(integer):");
-	scanf("%d", &secondNumber);
+	scanf("%f", &secondNumber);
 
-	if(firstNumber == secondNumber)
+	int32_t n1, n2;
+
+	n1 = firstNumber;
+	n2 = secondNumber;
+
+	if((n1 != firstNumber) || (n2 != secondNumber)){
+		printf("Warning ! comparing only integer part\n");
+	}
+
+	if(n1 == n2)
 	{
 		printf("The numbers are equal");
 	} else {
 
-		if(firstNumber > secondNumber)
+		if(n1 > n2)
 		{
-			printf("%d is bigger\n", firstNumber);
+			printf("%d is bigger\n", n1);
 		} else {
-			printf("%d is bigger\n", secondNumber);
+			printf("%d is bigger\n", n2);
 		}
 	}
 }
