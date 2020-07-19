87e25e0b17b7829d0f50f49dd82582a3ff0c92a4
Jon McKay <jmckay0411@gmail.com>
2020-07-19 17:47:53 -0600

    Modify LED exercise to use bit shifting as a faster mask creation method

--- STM32/Embedded-C/workspace/target/LED_exercise/Src/main.c
+++ STM32/Embedded-C/workspace/target/LED_exercise/Src/main.c
@@ -26,17 +26,17 @@ int main(void)
 	uint32_t  *pPortDModeReg = (uint32_t*) 0x40020C00;
 	uint32_t *pPortDOutReg = (uint32_t*) 0x40020C14;
 
-	// 1. enable the clock for GPIOD peripheral in the AHB1ENR
-	*pClkCtrlReg |= 0x08;
+	// 1. enable the clock for GPIOD peripheral in the AHB1ENR register (SET the 3rd bit position)
+	*pClkCtrlReg |= (1 << 3);
 
 	// 2. configure the mode of the IO pin as output
 	// a. clear the 24th and 25th bit positions (CLEAR)
-	*pPortDModeReg &= 0xFCFFFFFF;
+	*pPortDModeReg &= (3 << 24);
 	// b. make 24th bit position as 1 (SET)
-	*pPortDModeReg |= 0x01000000;
+	*pPortDModeReg |= (1 << 24);
 
 	// 3. SET 12th bit of the output data register to make I/O pin-12 as HIGH
-	*pPortDOutReg |= 0x1000;
+	*pPortDOutReg |= (1 << 12);
 
 	while(1);
 }
